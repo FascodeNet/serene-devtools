270455ad2aca0d1baf7f9f2dbb9dbc6898f20e2b
ksmt4699 <ksmt4699@gmail.com>
2019-11-04 14:07:10 +0900

    First commit

---
+++ createiso.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash -e
+if [ $UID != 0 ]; then
+    echo "You have to run this as root." 1>&2
+    exit 1
+fi
+CODENAME=develop
+VERSION=$(/home/serene/devtools/getversion.py)
+
+apt-get update && apt-get upgrade -y 
+
+echo "SereneLinux ${VERSION} \n \l" > base-files/etc/issue
+echo "SereneLinux $VERSION" > base-files/etc/issue.net
+sed -ie s/'DISTRIB_DESCRIPTION=\('.*'\)'/DISTRIB_DESCRIPTION=\"SereneLinux${VERSION}\"/g base-files/etc/lsb-release
+
+sed -ie s/'VERSION=\('.*'\)'/VERSION=\"${VERSION: -5} (${CODENAME})\"/g base-files/etc/os-release
+sed -ie s/'PRETTY_NAME=\('.*'\)'/PRETTY_NAME=\"SereneLinux${VERSION}\"/g base-files/etc/os-release
+sed -ie s/'VERSION_ID=\('.*'\)'/VERSION_ID=\"${VERSION: -5}\"/g base-files/etc/os-release
+sed -ie s/'VERSION_CODENAME=\('.*'\)'/VERSION_CODENAME=\"${CODENAME}\" base-files/etc/os-release
+
+sed -ie s/'Version=\('.*'\)'/Version=${VERSION: -5}/g base-files/etc/xdg/kcm-about-distrorc
+sed -ie s/'Variant=\('.*'\)'/Variant=${CODENAME} Release/g base-files/etc/xdg/kcm-about-distrorc
+
+sed -ie s/+serenelinux\.\*/+serenelinux19q4.6.5/g base-files/DEBIAN/control
+
+dpkg -b base-files
+
+apt-get autoremove --purge -y && apt-get clean
+rm -rf /var/cache/* \
+/var/log/* \
+/var/tmp/* \
+/var/crash/* \
+/var/backups/* \
+/tmp/* \
+/root/.bash_history \
+/home/serene/.bash_history \
+/home/serene/.cache/* \
---
+++ getversion.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import tkinter
+import tkinter.ttk
+import datetime
+import os
+import signal
+
+date = datetime.datetime.now()
+year = date.year
+quoter = -(-date.month // 3) - 1 if date.month >= 3 else 4
+
+root = tkinter.Tk()
+root.title("SereneLinux ISO Builder")
+root.geometry("300x50")
+
+#Label
+label = str(year)[2:] + "q" + str(quoter)
+Static1 = tkinter.Label(text=label + " . ")
+Static1.place(x=10, y=10)
+
+#Entry
+mejorver = tkinter.ttk.Combobox(root, state='readonly', width=1)
+mejorver["values"] = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+mejorver.current(0)
+mejorver.place(x=55, y=10)
+mejorver_piliod = tkinter.Label(text=" . ")
+mejorver_piliod.place(x=80, y=10)
+
+subver = tkinter.ttk.Combobox(root, state='readonly', width=1)
+subver["values"] = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+subver.current(0)
+subver.place(x=95, y=10)
+subver_piliod = tkinter.Label(text=" . ")
+subver_piliod.place(x=120, y=10)
+
+lastver = tkinter.ttk.Combobox(root, state='readonly', width=1)
+lastver["values"] = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+lastver.current(0)
+lastver.place(x=135, y=10)
+
+#Button
+def cleackd(root):
+    print(label + "." + mejorver.get() + "." + subver.get() + "." + lastver.get())
+    # Kill process
+    root.destroy()
+    
+button = tkinter.Button(text="OK", command=lambda : cleackd(root) )
+button.place(x=240, y=10)
+
+root.mainloop()
