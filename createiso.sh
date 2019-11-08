137263971c147fc5c3ae15b9fe8517c716adaa41
ksmt4699 <ksmt4699@gmail.com>
2019-11-08 20:53:16 +0900

    Bug fix

--- createiso.sh
+++ createiso.sh
@@ -6,23 +6,21 @@ fi
 CODENAME=develop
 VERSION=$(/home/serene/devtools/getversion.py)
 
-apt-get update && apt-get upgrade -y 
+apt-get update && apt-get upgrade -y
 
 echo "SereneLinux ${VERSION} \n \l" > base-files/etc/issue
 echo "SereneLinux $VERSION" > base-files/etc/issue.net
-sed -ie s/'DISTRIB_DESCRIPTION=\('.*'\)'/DISTRIB_DESCRIPTION=\"SereneLinux${VERSION}\"/g base-files/etc/lsb-release
-
-sed -ie s/'VERSION=\('.*'\)'/VERSION=\"${VERSION: -5} (${CODENAME})\"/g base-files/etc/os-release
-sed -ie s/'PRETTY_NAME=\('.*'\)'/PRETTY_NAME=\"SereneLinux${VERSION}\"/g base-files/etc/os-release
-sed -ie s/'VERSION_ID=\('.*'\)'/VERSION_ID=\"${VERSION: -5}\"/g base-files/etc/os-release
-sed -ie s/'VERSION_CODENAME=\('.*'\)'/VERSION_CODENAME=\"${CODENAME}\" base-files/etc/os-release
-
-sed -ie s/'Version=\('.*'\)'/Version=${VERSION: -5}/g base-files/etc/xdg/kcm-about-distrorc
-sed -ie s/'Variant=\('.*'\)'/Variant=${CODENAME} Release/g base-files/etc/xdg/kcm-about-distrorc
-
-sed -ie s/+serenelinux\.\*/+serenelinux19q4.6.5/g base-files/DEBIAN/control
+sed -ie s/DISTRIB_DESCRIPTION=\.*/DISTRIB_DESCRIPTION=\"SereneLinux${VERSION}\"/g base-files/etc/lsb-release
+sed -ie s/VERSION=\.\*/VERSION=\"${VERSION: -5} \(${CODENAME}\)\"/g base-files/etc/os-release
+sed -ie s/PRETTY_NAME=\.\*/PRETTY_NAME=\"SereneLinux${VERSION}\"/g base-files/etc/os-release
+sed -ie s/VERSION_ID=\.\*/VERSION_ID=\"${VERSION: -5}\"/g base-files/etc/os-release
+sed -ie s/VERSION_CODENAME=\.\*/VERSION_CODENAME=\"${CODENAME}\"/g base-files/etc/os-release
+sed -ie s/Version=\.\*/Version=${VERSION: -5}/g base-files/etc/xdg/kcm-about-distrorc
+sed -ie s/Variant=\.\*/Variant=${CODENAME} Release/g base-files/etc/xdg/kcm-about-distrorc
+sed -ie s/+serenelinux\.\*/+serenelinux${VERSION}/g base-files/DEBIAN/control
 
 dpkg -b base-files
+apt-get install -y ./base-files.deb
 
 apt-get autoremove --purge -y && apt-get clean
 rm -rf /var/cache/* \
@@ -33,4 +31,5 @@ rm -rf /var/cache/* \
 /tmp/* \
 /root/.bash_history \
 /home/serene/.bash_history \
-/home/serene/.cache/* \
+/home/serene/.cache/*
+bodhibuilder-gtk
