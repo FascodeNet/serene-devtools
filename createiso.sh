6de67f405c0c39665155be596a2fd845a995eb8e
ksmt4699 <ksmt4699@gmail.com>
2019-11-09 21:05:31 +0900

    Fix typo

--- createiso.sh
+++ createiso.sh
@@ -10,14 +10,14 @@ apt-get update && apt-get upgrade -y
 cd /home/serene/serene-devtools
 echo "SereneLinux ${VERSION} \n \l" > base-files/etc/issue
 echo "SereneLinux $VERSION" > base-files/etc/issue.net
-sed -ie s/'DISTRIB_DESCRIPTION=\.*'/DISTRIB_DESCRIPTION=\"SereneLinux${VERSION}\"/g base-files/etc/lsb-release
-sed -ie s/'VERSION=\.\*'/VERSION=\"${VERSION: -5} \(${CODENAME}\)\"/g base-files/etc/os-release
-sed -ie s/'PRETTY_NAME=\.\*'/PRETTY_NAME=\"SereneLinux${VERSION}\"/g base-files/etc/os-release
-sed -ie s/'VERSION_ID=\.\*'/VERSION_ID=\"${VERSION: -5}\"/g base-files/etc/os-release
-sed -ie s/'VERSION_CODENAME=\.\*'/VERSION_CODENAME=\"${CODENAME}\"/g base-files/etc/os-release
-sed -ie s/'Version=\.\*'/Version=${VERSION: -5}/g base-files/etc/xdg/kcm-about-distrorc
+sed -ie s/'DISTRIB_DESCRIPTION=.*'/DISTRIB_DESCRIPTION=\"SereneLinux${VERSION}\"/g base-files/etc/lsb-release
+sed -ie s/'VERSION=.*'/VERSION=\"${VERSION: -5} \(${CODENAME}\)\"/g base-files/etc/os-release
+sed -ie s/'PRETTY_NAME=.*'/PRETTY_NAME=\"SereneLinux${VERSION}\"/g base-files/etc/os-release
+sed -ie s/'VERSION_ID=.*'/VERSION_ID=\"${VERSION: -5}\"/g base-files/etc/os-release
+sed -ie s/'VERSION_CODENAME=.*'/VERSION_CODENAME=\"${CODENAME}\"/g base-files/etc/os-release
+sed -ie s/'Version=.*'/Version=${VERSION: -5}/g base-files/etc/xdg/kcm-about-distrorc
 sed -ie s/'Variant=.*'/"Variant=${CODENAME} Release"/g base-files/etc/xdg/kcm-about-distrorc
-sed -ie s/'+serenelinux\.\*'/+serenelinux${VERSION}/g base-files/DEBIAN/control
+sed -ie s/'+serenelinux.*'/+serenelinux${VERSION}/g base-files/DEBIAN/control
 
 dpkg -b base-files
 apt-get install -fy  ./base-files.deb
