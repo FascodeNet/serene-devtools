74a82ae426901caa83c80f3959502a45e580e0be
yangniao23 <yangniao23@gmail.com>
2020-03-18 19:25:08 +0900

    Update: ディレクトリoutの追加

--- make_base-files.sh
+++ make_base-files.sh
@@ -17,17 +17,3 @@ sed -i"" -e s!'BUG_REPORT_URL=.*'!BUG_REPORT_URL=\"https:\/\/serenelinux.net\/\"
 echo $WORKDIR
 sudo docker build -t builddeb ${WORKDIR}
 sudo docker run -e VERSION="$VERSION" -e BASE_FILES="$BASE_FILES" -e BASE_FILES_DIR="$BASE_FILES" -v ${WORKDIR}/out:/deb -it builddeb /debuild/debuild.sh
-#cd $BASE_FILES_DIR
-#dch -v ${BASE_FILES:11}serene${VERSION:5}
-#debuild -us -uc
-#cd ..
-#tar --remove-file -cf ${BASE_FILES}serene${VERSION:5}.tar \
-#base-files-dbgsym_${BASE_FILES:11}serene${VERSION:5}_amd64.ddeb \
-#base-files_${BASE_FILES:11}serene${VERSION:5}.dsc \
-#base-files_${BASE_FILES:11}serene${VERSION:5}.tar \
-#base-files_${BASE_FILES:11}serene${VERSION:5}.tar.xz \
-#base-files_${BASE_FILES:11}serene${VERSION:5}_amd64.build \
-#base-files_${BASE_FILES:11}serene${VERSION:5}_amd64.buildinfo \
-#base-files_${BASE_FILES:11}serene${VERSION:5}_amd64.changes \
-#base-files_${BASE_FILES:11}serene${VERSION:5}_amd64.deb \
-#lsb-release-udeb_${BASE_FILES:11}serene${VERSION:5}_all.udeb
