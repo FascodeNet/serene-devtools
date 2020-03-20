4ed487ee50c9e40c362b2e6ded41cfd896c00f16
yangniao23 <yangniao23@gmail.com>
2020-03-20 13:25:29 +0900

    Update: 処理の移行

--- debuild.sh
+++ debuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-if [ $UID != 0 ]; then
+if [ $UID != 65587 ]; then
     echo "You have to run this on Docker" 1>&2
     exit 1
 fi
@@ -7,6 +7,14 @@ fi
 export EDITOR=vim
 cd /debuild/build/$BASE_FILES_DIR
 
+
+echo "SereneLinux ${VERSION} \n \l" > etc/issue
+echo "SereneLinux $VERSION" > etc/issue.net
+sed -i"" -e s/'DISTRIB_DESCRIPTION=.*'/DISTRIB_DESCRIPTION=\"SereneLinux${VERSION}\"/g etc/lsb-release
+sed -i"" -e s/'PRETTY_NAME=.*'/PRETTY_NAME=\"SereneLinux${VERSION}\"/g etc/os-release
+sed -i"" -e s!'HOME_URL=.*'!HOME_URL=\"https\:\/\/serenelinux.com\/\"!g etc/os-release
+sed -i"" -e s!'SUPPORT_URL=.*'!SUPPORT_URL=\"https:\/\/twitter.com\/SereneDevJP\/\"!g etc/os-release
+sed -i"" -e s!'BUG_REPORT_URL=.*'!BUG_REPORT_URL=\"https:\/\/serenelinux.net\/\"!g etc/os-release
 cat <<EOF > changelog.new
 
 base-files (${BASE_FILES:11}serene${VERSION:5}) focal; urgency=medium
