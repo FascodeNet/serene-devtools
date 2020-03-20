d5354df0e160d4189795b1f1c9e965eaae87cda7
yangniao23 <yangniao23@gmail.com>
2020-03-20 13:18:40 +0900

    Update: base-filesの書き換え処理をDockerコンテナ内での実行に変更&Dockerコンテナを一般ユーザで動作されるよう変更

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 FROM ubuntu:20.04
 ENV VERSION base-files-11ubuntu4
+ENV UID 65587
 RUN sed -i"" -e 's%http://[^ ]\+%mirror://mirrors.ubuntu.com/mirrors.txt%g' /etc/apt/sources.list \
 && apt-get update \
 && apt-get -y upgrade \
-&& apt-get -y install build-essential devscripts zstd gawk libc6 libcrypt1 vim debhelper dh-systemd apt-utils ed
-RUN mkdir -p /debuild/build /deb
-ADD ./${ENV} /debuild/build/${ENV}
-ADD ./debuild.sh /debuild/debuild.sh
-RUN chown -R root:root /debuild \
-&& chmod +x /debuild/debuild.sh
-RUN rm -rf /tmp/* /var/tmp/* \
+&& apt-get -y install build-essential devscripts zstd gawk libc6 libcrypt1 debhelper dh-systemd apt-utils \
+&& rm -rf /tmp/* /var/tmp/* \
 && apt-get clean
+RUN useradd -m -u ${UID} docker 
+RUN mkdir -p /debuild/build /deb \
+&& chown -R docker:docker /debuild
+USER ${UID}
+ADD ./${VERSION} /debuild/build/${VERSION}
+ADD ./debuild.sh /debuild/debuild.sh
+RUN chmod +x /debuild/debuild.sh
 WORKDIR /debuild
-VOlUME out
 CMD ["./debuild.sh"]
--- make_base-files.sh
+++ make_base-files.sh
@@ -8,13 +8,5 @@ BASE_FILES=${BASE_FILES_DIR##*/}
 H_UID=${UID}
 H_GID=${GID}
 
-echo "SereneLinux ${VERSION} \n \l" > ${BASE_FILES_DIR}/etc/issue
-echo "SereneLinux $VERSION" > ${BASE_FILES_DIR}/etc/issue.net
-sed -i"" -e s/'DISTRIB_DESCRIPTION=.*'/DISTRIB_DESCRIPTION=\"SereneLinux${VERSION}\"/g ${BASE_FILES_DIR}/etc/lsb-release
-sed -i"" -e s/'PRETTY_NAME=.*'/PRETTY_NAME=\"SereneLinux${VERSION}\"/g ${BASE_FILES_DIR}/etc/os-release
-sed -i"" -e s!'HOME_URL=.*'!HOME_URL=\"https\:\/\/serenelinux.com\/\"!g ${BASE_FILES_DIR}/etc/os-release
-sed -i"" -e s!'SUPPORT_URL=.*'!SUPPORT_URL=\"https:\/\/twitter.com\/SereneDevJP\/\"!g ${BASE_FILES_DIR}/etc/os-release
-sed -i"" -e s!'BUG_REPORT_URL=.*'!BUG_REPORT_URL=\"https:\/\/serenelinux.net\/\"!g ${BASE_FILES_DIR}/etc/os-release
-
 sudo docker build -t builddeb ${WORKDIR}
 sudo docker run -e VERSION="$VERSION" -e BASE_FILES="$BASE_FILES" -e BASE_FILES_DIR="$BASE_FILES" -e H_UGID="${UID}:$(id -u)" -v ${WORKDIR}/out:/deb -it builddeb /debuild/debuild.sh
